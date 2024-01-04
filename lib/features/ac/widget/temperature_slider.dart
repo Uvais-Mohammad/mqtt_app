c172ee1cc060ded51364ab05926acf84e7f5198d
Uvais-Mohammad <uvaiskc55@gmail.com>
2024-01-04 11:53:29 +0530

    Add TemperatureSlider widget

---
+++ lib/features/ac/widget/temperature_slider.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:mqtt_app/features/ac/logic/air_condition/air_condition_bloc.dart';
+import 'package:sleek_circular_slider/sleek_circular_slider.dart';
+
+class TemperatureSlider extends StatelessWidget {
+  const TemperatureSlider({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 10),
+      child: SleekCircularSlider(
+        min: 16,
+        max: 30,
+        initialValue: 24,
+        appearance: CircularSliderAppearance(
+          customColors: CustomSliderColors(
+            trackColor: Colors.grey[300],
+            progressBarColors: [
+              const Color(0xFF1309FC),
+              const Color(0xFF9853D1),
+              const Color(0xFFFD8DB0),
+              const Color(0xFF9853D1),
+            ],
+            dotColor: Colors.red,
+            hideShadow: true,
+          ),
+          size: 270,
+          infoProperties: InfoProperties(
+            mainLabelStyle: const TextStyle(
+              color: Colors.black,
+              fontSize: 30,
+            ),
+          ),
+        ),
+        onChangeEnd: (double endValue) {
+          context.read<AirConditionBloc>().add(
+                AirConditionTemperatureChanged(
+                  endValue.toInt().toString(),
+                ),
+              );
+        },
+        innerWidget: (percentage) {
+          return Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  '${percentage.toInt()}°C',
+                  style: const TextStyle(
+                    color: Colors.black,
+                    fontSize: 30,
+                  ),
+                ),
+                const Text(
+                  'Slide to set temperature',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Color(0xFFA0A3B1),
+                    fontSize: 14,
+                  ),
+                ),
+              ],
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
