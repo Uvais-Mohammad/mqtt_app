8775a66a67f1b1b6b5a03374ab2119e7975c1407
Uvais-Mohammad <uvaiskc55@gmail.com>
2024-01-04 11:52:08 +0530

    Add AirConditionRepository to service locator

--- lib/service_locator.dart
+++ lib/service_locator.dart
@@ -1,10 +1,14 @@
-
 import 'package:get_it/get_it.dart';
+import 'package:mqtt_app/features/ac/repository/ac_repository.dart';
+import 'package:mqtt_app/features/ac/repository/i_ac_repository.dart';
 import 'package:mqtt_app/shared/services/i_mqtt_service.dart';
 import 'package:mqtt_app/shared/services/mqtt_service.dart';
 
 GetIt locator = GetIt.instance;
 
-void setupLocator() {
-  locator.registerLazySingleton<IMqttService>(() => MqttService());
+void setupLocator() async {
+  locator
+    ..registerLazySingleton<IMqttService>(() => MqttService())
+    ..registerLazySingleton<IAirConditionRepository>(
+        () => AirConditionRepository(mqttService: locator<IMqttService>()));
 }
