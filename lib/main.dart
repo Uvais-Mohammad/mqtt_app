eb026b57209a4e0cabe665044baf22dc36e13d03
Uvais-Mohammad <uvaiskc55@gmail.com>
2024-01-04 11:58:16 +0530

    Add app theme configuration

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:mqtt_app/features/ac/logic/air_condition/air_condition_bloc.dart';
 import 'package:mqtt_app/features/ac/repository/i_ac_repository.dart';
 import 'package:mqtt_app/features/splash/screens/splash_screen.dart';
 import 'package:mqtt_app/service_locator.dart';
 import 'package:mqtt_app/shared/services/i_mqtt_service.dart';
+import 'package:mqtt_app/shared/themes/themes.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -24,15 +24,7 @@ Future<void> main() async {
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
         home: const SplashScreen(),
-        theme: ThemeData(
-          textTheme: GoogleFonts.latoTextTheme(),
-          scaffoldBackgroundColor: const Color(0xffF6F9FE),
-          appBarTheme: const AppBarTheme(
-            backgroundColor: Color(0xffF6F9FE),
-            elevation: 0,
-            //
-          ),
-        ),
+        theme: appTheme,
       ),
     ),
   );
---
+++ lib/shared/themes/themes.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+ThemeData appTheme = ThemeData(
+  textTheme: GoogleFonts.latoTextTheme(),
+  scaffoldBackgroundColor: const Color(0xffF6F9FE),
+  appBarTheme: const AppBarTheme(
+    backgroundColor: Color(0xffF6F9FE),
+    elevation: 0,
+    //
+  ),
+);
